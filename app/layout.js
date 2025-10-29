31c4be4294c6f903bf156f5fd9071945e569fad0
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-10-29 09:55:16 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,14 +4,14 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Tesla',
-  description: 'Tesla Clone',
+title: 'Tesla',
+description: 'Tesla Clone',
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+return (
+<html lang="en">
+<body className={inter.className}>{children}</body>
+</html>
+)
 }
